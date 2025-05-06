fbe1629574ccf49c1bbeed1bb0e0586ed6fc2aa0
Sagar Dalwala <149292998+Sagar-Dalwala@users.noreply.github.com>
2025-05-07 00:33:47 +0530

    Update run_api.py

--- run_api.py
+++ run_api.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # Get configuration from environment variables or use defaults
 host = os.environ.get("API_HOST", "127.0.0.1")
-port = int(os.environ.get("API_PORT", 8000))
+port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
     print(f"Starting API server at http://{host}:{port}")
